c4b12d0afc2cd6758ad66a09867b8dec45ee2b56
akhi3030 <akhi3030@84f4ae0b-4b1d-0410-ab78-f247f7151164>
2006-09-21 00:32:46 +0000

    Now the binary to vhdl tool will generate the hex in proper format

--- tools/binToVhdl.c
+++ tools/binToVhdl.c
@@ -9,8 +9,10 @@ void processData (uint8_t input); //Function to process data and print it out pr
 int main ( int argc, char *argv[]) {
 
   FILE *fp; //File pointer to open the file
-  uint8_t input;
+  uint8_t input[DIGITS_PER_LINE / 2];
   unsigned long counter = 0;
+  int remainder;
+  int index;
 
   //Ensure right amounts of args received
   if ( argc != 2) {
@@ -23,16 +25,33 @@ int main ( int argc, char *argv[]) {
 
   //Print out the binary file
   while(feof(fp) == 0) {
-    input = fgetc(fp);
-    processData(input);
+    //Grab the input from the file
+    input[counter % (DIGITS_PER_LINE / 2)] = fgetc(fp);
     counter++;
 
+    //If we have grabbed enough input for one line
     if (counter % (DIGITS_PER_LINE / 2) == 0) {
+      for (index = (DIGITS_PER_LINE / 2 - 1); index > -1; index--) {
+	processData(input[index]);
+      }
       printf("\n");
     }
   }
 
+  //Fill the remaining input of the line
+  remainder = DIGITS_PER_LINE / 2 - (counter % (DIGITS_PER_LINE / 2));
+
+  for (index = 0; index < remainder; index++) {
+    printf("ff");
+  }
+  for (index = (counter % (DIGITS_PER_LINE / 2) - 1); index > -1; index--) {
+    processData(input[index]);
+  }
+
+  //Increment the counter appropriately
+  counter = counter + remainder;
   
+  /*  
   //Fill the rest with FF data
   while(counter < MAX_MEMORY) {
     printf("FF");
@@ -41,19 +60,45 @@ int main ( int argc, char *argv[]) {
       printf("\n");
     }
   }
-  
+  */
   return 0;
 }
 
 void processData (uint8_t input) {
-  //Print the first digit
-  if (input / 16 > 0) {
-    printf("%x",input / 16);
-  }
-  else {
-    printf("0");
-  }
-  //Print the second digit
-  printf("%x",input % 16);
   
+  //Print the first digit
+  if (input % 16 == 0) printf("0");
+  else if(input % 16 == 1) printf("8");
+  else if(input % 16 == 2) printf("4");
+  else if(input % 16 == 3) printf("c");
+  else if(input % 16 == 4) printf("2");
+  else if(input % 16 == 5) printf("a");
+  else if(input % 16 == 6) printf("6");
+  else if(input % 16 == 7) printf("e");
+  else if(input % 16 == 8) printf("1");
+  else if(input % 16 == 9) printf("9");
+  else if(input % 16 == 10) printf("5");
+  else if(input % 16 == 11) printf("d");
+  else if(input % 16 == 12) printf("3");
+  else if(input % 16 == 13) printf("b");
+  else if(input % 16 == 14) printf("7");
+  else if(input % 16 == 15) printf("f");
+
+  //Print the second digit
+  if (input / 16 == 0) printf("0");
+  else if(input / 16 == 1) printf("8");
+  else if(input / 16 == 2) printf("4");
+  else if(input / 16 == 3) printf("c");
+  else if(input / 16 == 4) printf("2");
+  else if(input / 16 == 5) printf("a");
+  else if(input / 16 == 6) printf("6");
+  else if(input / 16 == 7) printf("e");
+  else if(input / 16 == 8) printf("1");
+  else if(input / 16 == 9) printf("9");
+  else if(input / 16 == 10) printf("5");
+  else if(input / 16 == 11) printf("d");
+  else if(input / 16 == 12) printf("3");
+  else if(input / 16 == 13) printf("b");
+  else if(input / 16 == 14) printf("7");
+  else if(input / 16 == 15) printf("f");
 }
